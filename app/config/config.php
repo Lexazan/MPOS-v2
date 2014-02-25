273b9aa93fa561ed0f37930baf114386c464b677
Sebastian Grewe <sebastian@grewe.ca>
2014-02-25 12:15:15 +0100

    [ADDED] Default Phalcon project layout
    Fixes #1

---
+++ app/config/config.php
@@ -0,0 +1,20 @@
+<?php
+
+return new \Phalcon\Config(array(
+    'database' => array(
+        'adapter'     => 'Mysql',
+        'host'        => '127.0.0.1',
+        'username'    => 'someuser',
+        'password'    => 'somepass',
+        'dbname'      => 'mposv2',
+    ),
+    'application' => array(
+        'controllersDir' => __DIR__ . '/../../app/controllers/',
+        'modelsDir'      => __DIR__ . '/../../app/models/',
+        'viewsDir'       => __DIR__ . '/../../app/views/',
+        'pluginsDir'     => __DIR__ . '/../../app/plugins/',
+        'libraryDir'     => __DIR__ . '/../../app/library/',
+        'cacheDir'       => __DIR__ . '/../../app/cache/',
+        'baseUri'        => '/MPOS-v2/',
+    )
+));
